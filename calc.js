34e31a39579ae30a9433e2b28bb8b19b4db90718
urmika2002 <111737539+urmika2002@users.noreply.github.com>
2023-03-23 15:02:25 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,28 @@
+function add(x, y) {
+    return x + y;
+};
+function square(x){
+        return x*x;
+};
+function sub(x, y) {
+    return x - y;
+};
+    
+function mult(x, y) {
+    return x * y;
+};
+function div(x, y) {
+    return x / y;
+};
+function square(x){
+    return x*x;
+}
+function cube(x){
+    return x*x*x;
+}
+exports.add = add;
+exports.sub = sub;
+exports.mult = mult;
+exports.div = div;
+exports.square =square;
+exports.cube=cube;
